3953e11461139caa9bfbd77a76c3b764a165aa84
matt-sanpedro <mattsanpedro76@gmail.com>
2020-10-15 09:29:29 -0700

    completed subtract challenge

--- scratch.js
+++ scratch.js
@@ -7,6 +7,10 @@ function multiply(num1, num2) {
   return num1 * num2;
 }
 
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
 function calculator(num1, num2, operator) {
   return operator(num1, num2);
 }
