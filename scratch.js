88e2488639ecc5f21befa50dea4d48cc72a9f22b
matt-sanpedro <mattsanpedro76@gmail.com>
2020-10-15 00:10:02 -0700

    added function within function example

---
+++ scratch.js
@@ -0,0 +1,19 @@
+// paste code into web browser console to see the action
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function calculator(num1, num2, operator) {
+  return operator(num1, num2);
+}
+
+let result = calculator(2, 3, multiply);
+
+console.log(result);
+
+debugger;
+calculator(2, 3, multiply);
