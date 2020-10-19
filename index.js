c0af59e19f422dda5ae513aca4c385818f624b63
matt-sanpedro <mattsanpedro76@gmail.com>
2020-10-18 22:47:33 -0700

    refactored to playSound function and added press keyboard for sound functionality

--- index.js
+++ index.js
@@ -13,56 +13,72 @@ const btns = document.querySelectorAll(".drum");
 // });
 
 // solution from Udemy
-// var audio = new Audio("sounds/snare.mp3");
-// audio.play();
+function playSound(keySound) {
+  switch (keySound) {
+    case "w":
+      // console.log("Clicked w!");
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "a":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "s":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "d":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "j":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "k":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "l":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    default:
+      console.log(keySound);
+  }
+}
+
+// event listener: for clicking the sound button
 for (i = 0; i < btns.length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    // alert("I got clicked!");
-    // console.log(this);
-    // this.style.color = "white";
+      // alert("I got clicked!");
+      // console.log(this);
+      // this.style.color = "white";
 
-    var buttonInnerHTML = this.innerHTML;
-    // console.log(buttonInnerHTML);
+      var buttonInnerHTML = this.innerHTML;
+      // console.log(buttonInnerHTML);
 
-    switch (buttonInnerHTML) {
-      case "w":
-        // console.log("Clicked w!");
-        var tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case "a":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case "s":
-        var kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-
-      case "d":
-        var tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case "j":
-        var tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      case "k":
-        var snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-
-      case "l":
-        var crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      default: console.log(buttonInnerHTML);
+      playSound(buttonInnerHTML);
 
     }
-  });
+  );
 }
+
+// event listener: for pressing the keyboard button
+// listen for all keystrokes with "document"
+document.addEventListener("keydown", function (event) {
+  // console.log("Key was pressed");
+  // console.log(event);
+  var keyPressed = event.key;
+
+  playSound(keyPressed);
+
+});
